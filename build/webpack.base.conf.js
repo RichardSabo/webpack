17203e39e27bb6148e076b01e3a0f4a7c2a06ad3
RichardSabo <richardsabo333@gmail.com>
2021-02-11 21:18:32 +0200

    clean dist folder

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,83 +1,95 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const PATHS = {
-  src: path.join(__dirname, '../src'),
-  dist: path.join(__dirname, '../dist'),
-  assets: 'assets/'
-}
+  src: path.join(__dirname, "../src"),
+  dist: path.join(__dirname, "../dist"),
+  assets: "assets/",
+};
 
 module.exports = {
-
   externals: {
-    paths: PATHS
+    paths: PATHS,
   },
 
   entry: {
-    app: PATHS.src
+    app: PATHS.src,
   },
   output: {
     filename: `${PATHS.assets}js/[name].js`,
     path: PATHS.dist,
-    publicPath: '/'
+    publicPath: "/",
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: '/node_modules'
-    }, {
-      test: /\.(png|jpg|gif|svg)$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    }, {
-      test: /\.less$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: "css-loader",
-          options: { sourceMap: true }
-        }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
-        }, {
-          loader: "less-loader",
-          options: { sourceMap: true }
-        }
-      ]
-    }, {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: "css-loader",
-          options: { sourceMap: true }
-        }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js`} }
-        }
-      ]
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules",
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+        },
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              config: { path: `${PATHS.src}/js/postcss.config.js` },
+            },
+          },
+          {
+            loader: "less-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              config: { path: `${PATHS.src}/js/postcss.config.js` },
+            },
+          },
+        ],
+      },
+    ],
   },
 
   plugins: [
     new MiniCssExtractPlugin({
-      filename: `${PATHS.assets}css/[name].css`
+      filename: `${PATHS.assets}css/[name].css`,
     }),
     new HtmlWebpackPlugin({
       hash: false,
       template: `${PATHS.src}/index.html`,
-      filename: './index.html'
+      filename: "./index.html",
     }),
     new CopyWebpackPlugin([
-      { from: `${PATHS.src}/img`, to: `${PATHS.assets}img`},
-      { from: `${PATHS.src}/static`, to: ``}
-      ])
-  ]
-}
+      { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
+    ]),
+  ],
+};
--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,11 +1,12 @@
-const baseWebpackConfig = require('./webpack.base.conf')
-const merge = require('webpack-merge')
+const baseWebpackConfig = require("./webpack.base.conf");
+const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const buildWebpackConfig = merge(baseWebpackConfig,{
-	mode: 'production',
-	plugins: []
-})
+const buildWebpackConfig = merge(baseWebpackConfig, {
+  mode: "production",
+  plugins: [new CleanWebpackPlugin()],
+});
 
-module.exports = new Promise((resolve,reject) => {
-	resolve(buildWebpackConfig)
-})
+module.exports = new Promise((resolve, reject) => {
+  resolve(buildWebpackConfig);
+});
